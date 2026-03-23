98a75761d3d7f1c3764fbe4ffe9701bc7416b83c
McCahen <marcel.cahenzli@gmail.com>
2026-03-23 21:01:52 +0100

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,4 +1,3 @@
 function toggleMenu() {
-    const nav = document.getElementById('main-nav');
-    nav.classList.toggle('active');
+    document.getElementById('main-nav').classList.toggle('active');
 }
